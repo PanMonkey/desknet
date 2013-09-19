94b37e8d2fbc2fa91f7128af7d947345cea2c264
lunkhead10 <lunkhead10@gmail.com>
2013-09-19 23:06:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Desknet::Application.config.secret_key_base = '644b97b2d94362309265f78cfc9934f9c3ff4801d55d6035762a92f25666393030a58bed423c3e9ee3ac4f8c3644237e9e9a8fca445943393152e767b9fc3626'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Desknet::Application.config.session_store :cookie_store, key: '_desknet_session'
